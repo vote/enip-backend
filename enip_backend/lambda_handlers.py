4d801b48317181219d983eae309b059b43d51ae6
Matteo Banerjee <matteobanerjee@gmail.com>
2020-10-14 19:18:18 -0400

    Set up Sentry, couple serverless fixes

--- enip_backend/lambda_handlers.py
+++ enip_backend/lambda_handlers.py
@@ -1,2 +1,11 @@
+import sentry_sdk
+
+from .enip_common import config
+
+
+if config.SENTRY_DSN:
+    sentry_sdk.init(config.SENTRY_DSN, environment=config.SENTRY_ENVIRONMENT)
+
+
 def hello(event, context):
     print("Hello ENIP")
