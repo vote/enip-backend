ec9d03a38c4219a91bd91e40e1da0999ffe00cd9
Matteo Banerjee <matteobanerjee@gmail.com>
2020-10-14 19:44:38 -0400

    Add sentry lambda integration

--- enip_backend/lambda_handlers.py
+++ enip_backend/lambda_handlers.py
@@ -1,10 +1,15 @@
 import sentry_sdk
+from sentry_sdk.integrations.aws_lambda import AwsLambdaIntegration
 
 from .enip_common import config
 
 
 if config.SENTRY_DSN:
-    sentry_sdk.init(config.SENTRY_DSN, environment=config.SENTRY_ENVIRONMENT)
+    sentry_sdk.init(
+        config.SENTRY_DSN,
+        environment=config.SENTRY_ENVIRONMENT,
+        integrations=[AwsLambdaIntegration()]
+    )
 
 
 def hello(event, context):
